0d08b2425de94364e66bfb730cabacc824c4d710
PHOOM(Office) <rungreung.sak@gmail.com>
2021-12-21 15:04:34 +0700

    finished faling Snow

--- 01_Falling_Snow/start.js
+++ 01_Falling_Snow/start.js
@@ -1,7 +1,7 @@
 (() => {
   // เริ่มเขียนโค้ด
   function setup() {
-    const canvas = document.getElementById("falling-snow-canvas"); //get element canvas
+    const canvas = document.getElementById("l"); //get element canvas
     canvas.width = window.innerWidth; //setup backgroung-img
     canvas.height = window.innerHeight; //setup backgroung-img
 
@@ -12,31 +12,65 @@
       numberOfSnowBall: 250,
     };
   }
+
   function random(min, max) {
     //random position snowBall
-    return Math.floor(Math.random() * (max - min + 1) + min);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+
   function createSnowBalls(canvas, numberOfSnowBall) {
     return [...Array(numberOfSnowBall)].map(() => {
       return {
         x: random(0, canvas.width), //random position
         y: random(0, canvas.height), //random position
+        opacity: random(0.5, 1), //random opacity
+        radius: random(2, 4), //random size showBall
+        speedX: random(-5, 5), // random speedx
+        speedY: random(1, 3), //radmom speedy
       };
     });
   }
 
   function drawSnowBall(canvasContext, snowBalls) {
     canvasContext.beginPath();
-    canvasContext.arc(snowBalls.x, snowBalls.y, 4, 0, Math.PI * 2);
-    canvasContext.fillStyle = `rgba(255,255,255,1)`;
+    canvasContext.arc(
+      snowBalls.x,
+      snowBalls.y,
+      snowBalls.radius,
+      0,
+      Math.PI * 2
+    );
+    canvasContext.fillStyle = `rgba(255,255,255,${snowBalls.opacity})`;
     canvasContext.fill();
+  }
+  function moveSnowBall(canvas, snowBall) {
+    snowBall.x += snowBall.speedX;
+    snowBall.y += snowBall.speedY;
+    if (snowBall.x > canvas.width) {
+      //ตกขอบ x set position ใหม่
+      snowBall.x = 0;
+    } else if (snowBall.x < 0) {
+      snowBall.x = canvas.width;
+    }
+
+    if (snowBall.y > canvas.height) {
+      //ตกขอบ y set position ใหม่
+      snowBall.y = 0;
+    } else if (snowBall.y < 0) {
+      snowBall = canvas.height;
+    }
   }
 
   function run() {
-    const { canvas, canvasContext, numberOfSnowBall } = setup();
-    const snowBalls = createSnowBalls(canvas, numberOfSnowBall);
-    console.log(snowBalls);
-    drawSnowBall(canvasContext, snowBalls[1]);
+    const { canvas, canvasContext, numberOfSnowBall } = setup(); //ดึงมาเตรียมใช้
+    const snowBall = createSnowBalls(canvas, numberOfSnowBall); //crateShowBall
+
+    setInterval(() => {
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
+      snowBall.forEach((snowBall) => drawSnowBall(canvasContext, snowBall)); // show show ball
+      snowBall.forEach((snowBall) => moveSnowBall(canvas, snowBall)); // move snowBall
+    }, 50);
   }
   run();
 })();
